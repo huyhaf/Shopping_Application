15a1766f88890fe584b999fc62929a5d3287c805
huyhaf <huyhn303@gmail.com>
2024-12-24 16:34:48 +0700

    adding models and repos in be

---
+++ shopapp-backend/shopapp/src/main/java/com/example/shopapp/models/Role.java
@@ -0,0 +1,20 @@
+package com.example.shopapp.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", length = 20,nullable = false)
+    private String name;
+}
---
+++ shopapp-backend/shopapp/src/main/java/com/example/shopapp/models/SocialAccount.java
@@ -0,0 +1,31 @@
+package com.example.shopapp.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "social_accounts")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SocialAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "provider", length = 20,nullable = false)
+    private String provider;
+
+    @Column(name = "provider_id", length = 50)
+    private String providerId;
+
+    @Column(name = "email", length = 150)
+    private String email;
+
+    @Column(name = "name", length = 100)
+    private String name;
+
+
+}
