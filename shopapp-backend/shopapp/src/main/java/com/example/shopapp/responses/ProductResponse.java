69dd136158a783182eafd9321bb0d5baec2c2061
huyhaf <huyhn303@gmail.com>
2025-03-29 17:51:29 +0700

    update api for backend

--- shopapp-backend/shopapp/src/main/java/com/example/shopapp/responses/ProductResponse.java
+++ shopapp-backend/shopapp/src/main/java/com/example/shopapp/responses/ProductResponse.java
@@ -1,5 +1,6 @@
 package com.example.shopapp.responses;
 
+import com.example.shopapp.models.Product;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
@@ -10,10 +11,22 @@ import lombok.*;
 @Builder
 public class ProductResponse extends BaseResponse{
     private String name;
-    private float price;
+    private Float price;
     private String thumbnail;
     private String description;
 
     @JsonProperty("category_id")
     private Long categoryId;
+    public static ProductResponse fromProduct(Product product) {
+        ProductResponse productResponse = ProductResponse.builder()
+                .name(product.getName())
+                .price(product.getPrice())
+                .thumbnail(product.getThumbnail())
+                .description(product.getDescription())
+                .categoryId(product.getCategory().getId())
+                .build();
+        productResponse.setCreatedAt(product.getCreatedAt());
+        productResponse.setUpdatedAt(product.getUpdatedAt());
+        return productResponse;
+    }
 }
